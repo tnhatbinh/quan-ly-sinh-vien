4f95638b7deb155083a215a07df90f9568106dd6
Binh <nhatbinh6969@gmail.com>
2026-09-21 02:19:37 +0700

    lan 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/quan-ly-sinh-vien/", // Thay bằng đúng tên kho lưu trữ của ông
+});
